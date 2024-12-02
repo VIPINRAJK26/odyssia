dc8aab5d0091700a9932721fa956ebc351bddd06
VIPINRAJK26 <vipinrajk026@gmail.com>
2024-12-02 14:06:54 +0530

    responsive

--- src/components/home/Banner.jsx
+++ src/components/home/Banner.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Banner = () => {
   return (
-    <div className="grid grid-cols-1 md:grid-cols-2 gap-0 lg:px-32 px-3 pb-14">
+    <div className="container mx-auto grid grid-cols-1 md:grid-cols-2 gap-0 lg:px-32 px-3 pb-14">
       <div>
         <img
           src="https://img.freepik.com/free-vector/modern-sale-banner-with-product_1361-1636.jpg?ga=GA1.1.1208105082.1712396076&semt=ais_hybrid"
--- src/components/home/ElementorSection.jsx
+++ src/components/home/ElementorSection.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const ElementorSection = () => {
   return (
-    <div className="grid grid-cols-1 md:grid-cols-3 px-3  gap-6 mt-10 container lg:px-32">
+    <div className="grid grid-cols-1 md:grid-cols-3 px-3  gap-6 mt-10 container mx-auto lg:px-32">
       <div className="elementor-img hover:translate-x-3 ease-in-out duration-1000">
         <img
           src="https://odyssia.in/wp-content/uploads/2024/06/COLORS-OF-ODYSSIA-FOR-WEBSITE-1024X1024.webp"
